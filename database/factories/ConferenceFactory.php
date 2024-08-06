0bfe07a492f4a843dceb65711edde1d2ca16718f
takamori minami <minami@takamorinoMacBook-Pro.local>
2024-08-07 01:48:55 +0900

    edit_dummydata

--- database/factories/ConferenceFactory.php
+++ database/factories/ConferenceFactory.php
@@ -16,10 +16,24 @@ class ConferenceFactory extends Factory
      */
     public function definition(): array
     {
+        // 範囲は10時-20時
+        $availableHour = $this->faker->numberBetween(10, 20);
+        // 区切り-00分・30分
+        $minutes = [0,30];
+        // ランダムにkeyを取得
+        $mKey = array_rand($minutes);
+        // イベント時間 1時間-3時間
+        $addHour = $this->faker->numberBetween(1,3);
+
         // faker チートシート参照
         
         // 今月分の日時指定用の変数
         $dummyDate = $this->faker->dateTimeThisMonth;
+        $startDate = $dummyDate->setTime($availableHour, $minutes[$mKey]);
+        $clone = clone $startDate;
+        $endDate = $clone->modify('+'.$addHour.'hour');
+        // 出力確認
+        // dd($startDate, $endDate);
 
         return [
             //ダミーデータの作成
@@ -28,8 +42,8 @@ class ConferenceFactory extends Factory
             'max_people' => $this->faker->numberBetween(1,20),
             // 以下の2つのcolumnは作成した「$dummyDate」にて設定
             // end_dateはmodifyにて1時間追加の日時にて設定
-            'start_date' => $dummyDate->format('Y-m-d H:i:s'),
-            'end_date' => $dummyDate->modify('+1hour')->format('Y-m-d H:i:s'),
+            'start_date' => $startDate,
+            'end_date' => $endDate,
             'is_visible' => $this->faker->boolean,
         ];
     }
