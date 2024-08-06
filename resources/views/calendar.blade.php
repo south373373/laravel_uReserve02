7203c9d2ebfde72f4e377ecd76e18ee1b917a4e7
takamori minami <minami@takamorinoMacBook-Pro.local>
2024-08-07 02:53:24 +0900

    make_components-calendar-time_day

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -5,11 +5,10 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-4">
         <!-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8"> -->
-        <div class="max-w-8xl mx-auto sm:px-6 lg:px-20">
+        <div class="event-calendar border border-red-400 mx-auto sm:px-6 lg:px-20">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">                
-                <div class="max-w-2xl py-4 mx-auto">
                 <!-- resources > calendar > indexの読み込み -->
                 <!-- 変数を定義していない状態のviewを表示する場合 -->
                 {{-- <x-calendar /> --}}
@@ -17,7 +16,6 @@
                 <!-- 変数を定義している状態のviewを表示する場合 -->
                 <x-calendar :currentDate="$currentDate" :currentWeek="$currentWeek" />        
                 <!-- @livewireScriptsは無しで設定 -->
-                </div>
             </div>
         </div>
     </div>
--- resources/views/components/calendar.blade.php
+++ resources/views/components/calendar.blade.php
@@ -1,8 +1,22 @@
 <div>
-    カレンダー
+    <div class="text-center text-sm">
+        日付を選択してください。本日から最大30日先まで選択可能です。
+    </div>
     <!-- x-text-input -->
-        <input id="calendar" class="block mt-1 w-full" type="text" name="calendar" />
+        <input id="calendar" class="block mt-1 mb-2 mx-auto" type="text" name="calendar" />
         {{ $currentDate }}
+    <!-- カレンダーの体裁 -->
+    <div class="flex border border-green-400 mx-auto">
+        <x-calendar-time />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+        <x-day />
+    </div>
+
     <div id="week-display" class="flex">
         @foreach($currentWeek as $day)
             {{ $day }}
